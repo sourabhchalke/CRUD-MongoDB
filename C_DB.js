0209cb3afc897da589b4490fe123046836ce1201
sourabhchalke <sourabhchalke2157@gmail.com>
2024-08-22 21:38:01 +0530

    collection added

--- C_DB.js
+++ C_DB.js
@@ -4,6 +4,9 @@
 use myDatabase;//It uses this database if exist ,if not then it will first create database automatically and then it uses database;
 //switch to db myDatabase
 
-show databases; //it will show all databases which is available
+show databases; //it will not show database , until you insert at least one document
 
+db.employee.insert({id:1,name:"Virat",email:"virat18@gmail.com",address:"Delhi"})//after inserting this it will show database 
+//And also it will create collection automatically after inserting data.
 
+show collections //this will show all connections contain within this particular database
