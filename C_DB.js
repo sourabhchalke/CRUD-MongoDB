6d826cd2ad4bee95f1be36bb8c696bc47b6e68df
sourabhchalke <sourabhchalke2157@gmail.com>
2024-08-22 21:18:34 +0530

    Create and Use database

---
+++ C_DB.js
@@ -0,0 +1,9 @@
+// In MongoDB you can create collection manually by 'db.createCollection()' but not the database.
+
+
+use myDatabase;//It uses this database if exist ,if not then it will first create database automatically and then it uses database;
+//switch to db myDatabase
+
+show databases; //it will show all databases which is available
+
+
